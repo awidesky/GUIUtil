6c4cd2b8ca8b970794177fff93363deddf9f4e49
awidesky <awidesky@naver.com>
2022-07-17 23:08:28 +0900

    add log(Object... objs)

--- src/logging/LoggerThread.java
+++ src/logging/LoggerThread.java
@@ -3,6 +3,7 @@ package logging;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
@@ -11,9 +12,12 @@ public class LoggerThread extends Thread { //TODO : add prefix
 	private PrintWriter logTo;
 	private LinkedBlockingQueue<Runnable> loggerQueue = new LinkedBlockingQueue<>();
 	
+	private SimpleDateFormat datePrefix = null;
+	private String prefix = null;
+	
 	public volatile boolean isStop = false;
 	
-	public LoggerThread(OutputStream os) {
+	public LoggerThread(OutputStream os) { //TODO : default construct that use sysout
 		this(os, true, Charset.defaultCharset());
 	}
 	
@@ -53,19 +57,43 @@ public class LoggerThread extends Thread { //TODO : add prefix
 	
 	public void log(String data) {
 
-		loggerQueue.offer(() -> {
-			logTo.println(data.replaceAll("\\R", System.lineSeparator()));
-		});
+		loggerQueue.offer(getLogTask(data));
 		
 	}
 	
 	public void log(Exception e) {
 		
-		loggerQueue.offer(() -> {
-			e.printStackTrace(logTo);
-		});
+		loggerQueue.offer(getLogTask(e));
 		
 	}
+	
+	public void log(Object... objs) {
+		loggerQueue.offer(() -> {
+			for(Object o : objs) getLogTask(o).run();
+		});
+	}
+	
+	
+	private Runnable getLogTask(String data) {
+		return () -> {
+			logTo.println(data.replaceAll("\\R", System.lineSeparator()));
+		};
+	}
+	
+	private Runnable getLogTask(Exception e) {
+		return () -> {
+			e.printStackTrace(logTo);
+		};
+	}
+	
+	private Runnable getLogTask(Object obj) {
+		return () -> {
+			logTo.println(obj.toString().replaceAll("\\R", System.lineSeparator()));
+		};
+	}
+	
+
+	
 	
 	/**
 	 * Kill LoggerThread in <code>timeOut</code> ms.
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,9 +1,20 @@
 package test;
 
+import java.util.ArrayList;
+
+import logging.LoggerThread;
+
 public class Test {
 
 	public static void main(String[] args) {
 
+		LoggerThread l = new LoggerThread(System.out);
+		l.start();
+		l.log(new ArrayList<String>(), " dfsdf", new Exception("dfasdfa"));
+		l.log("ttttt");
+		l.log(new Object());
+		l.kill(1000);
+		
 	}
 
 }

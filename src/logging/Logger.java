5dd884088703cdc1d4df84045b867aa2237764c6
awidesky <awidesky@naver.com>
2022-10-03 11:16:53 +0900

    add nullLogger

--- src/logging/Logger.java
+++ src/logging/Logger.java
@@ -2,6 +2,13 @@ package logging;
 
 public interface Logger {
 
+	public static final Logger nullLogger = new Logger() {
+		@Override
+		public void log(String data) {}
+		@Override
+		public void log(Exception e) {}
+	};
+	
 	public void log(String data);
 	public void log(Exception e);
 	

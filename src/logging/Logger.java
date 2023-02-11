3cc4fa8808870f1838f48846ed0a8076bd272d3a
Eugene Hong <awidesky@naver.com>
2023-02-11 23:30:23 +0900

    add comment

--- src/logging/Logger.java
+++ src/logging/Logger.java
@@ -4,11 +4,12 @@ import java.text.DateFormat;
 
 
 /**
+ * An Onterface for a Logger object.
+ * 
  * @author Eugene Hong
  * */
 public interface Logger {
 
-	
 	/**
 	 * Set date information prefix for this <code>Logger</code> instance.
 	 * if argument is <code>null</code>, no date information prefix is appended,
@@ -21,13 +22,32 @@ public interface Logger {
 	 * */
 	public void setPrefix(String prefix);
 	
+	/**
+	 * Print a newLine character. 
+	 * */
 	public void newLine();
 
+	/**
+	 * Logs String <code>data</code>
+	 * */
 	public void log(String data);
+	/**
+	 * Logs an <code>Exception</code> 
+	 * */
 	public void log(Exception e);
+	/**
+	 * Logs an array of <code>Object</code>s
+	 * */
 	public void log(Object... objs);
 	
-	public void logVerbose(String data);
-	public void setVerbose(boolean verbose);
 	
+	/**
+	 * Set verbosity of this <code>Logger</code> object.
+	 * */
+	public void setVerbose(boolean verbose);
+	/**
+	 * Log in verbose mode.
+	 * If <code>setVerbose</code> is <code>true</code>, argument <code>data</code> is logged, otherwise it doesn't 
+	 * */
+	public void logVerbose(String data);
 }

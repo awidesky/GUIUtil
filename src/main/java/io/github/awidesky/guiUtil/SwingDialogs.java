205971e1fd46c745abbb1a19dd469527b3395952
Eugene Hong <awidesky@naver.com>
2023-08-27 16:09:33 +0900

    add input and inputPassword methods

--- src/main/java/io/github/awidesky/guiUtil/SwingDialogs.java
+++ src/main/java/io/github/awidesky/guiUtil/SwingDialogs.java
@@ -9,14 +9,22 @@
 
 package io.github.awidesky.guiUtil;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.SwingUtilities;
+
 
 /**
  * An utility class that has static methods for various dialog functionality like 
@@ -34,7 +42,9 @@ public class SwingDialogs {
 
 	private static Logger logger = Logger.consoleLogger;
 
-	
+	/**
+	 * Set Logger for all SwingDialog operation.
+	 * */
 	public static void setLogger(Logger newLogger) {
 		logger = newLogger;
 	}
@@ -241,10 +251,142 @@ public class SwingDialogs {
 		return result;
 	}
 	
+	
+	/**
+	 * Show an input dialog.
+	 * 
+	 * @return User's answer. If user canceled, {@code null}.
+	 * */
+	public static String input(String title, String prompt) {
+		logger.log("[SwingDialogs.input] " + title + "\n\t" + prompt);
+		String ret = showInputDialog(title, prompt);
+		logger.log("[SwingDialogs.input] Input was : " + ret);
+		return ret;
+	}
+	
+	/**
+	 * Show input dialog.
+	 * this method returns after the dialog closed.
+	 * */
+	private static String showInputDialog(String title, String prompt) {
+
+		final JDialog dialog = createDialog();
+		
+		if (EventQueue.isDispatchThread()) {
+
+			return JOptionPane.showInputDialog(dialog, prompt.replace("\n", System.lineSeparator()), title.replace("\n", System.lineSeparator()), JOptionPane.QUESTION_MESSAGE);
+			
+		} else {
+			AtomicReference<String> ret = new AtomicReference<>();
+			try {
+				SwingUtilities.invokeAndWait(() -> {
+					ret.set(JOptionPane.showInputDialog(dialog, prompt.replace("\n", System.lineSeparator()), title.replace("\n", System.lineSeparator()), JOptionPane.QUESTION_MESSAGE));
+				});
+				return ret.getAcquire();
+			} catch (Exception e) {
+				error("Exception in Thread working(SwingWorker)", "%e%", (e instanceof InvocationTargetException) ? (Exception)e.getCause() : e, false);
+				return "";
+			}
+
+		}
+		
+	}
+	
+	/**
+	 * Show a password input dialog.
+	 * 
+	 * @return Received password. If user canceled, {@code null}.
+	 * */
+	public static char[] inputPassword(String title, String prompt) {
+		logger.log("[SwingDialogs.inputPassword] " + title + "\n\t" + prompt);
+		return showInputPasswordDialog(title, prompt);
+	}
+	
+	/**
+	 * Show password input dialog.
+	 * this method returns after the dialog closed.
+	 * 
+	 * code from https://stackoverflow.com/a/8881370
+	 * */
+	private static char[] showInputPasswordDialog(String title, String prompt) {
+		
+		PasswordPanel pPnl = new PasswordPanel(prompt.replace("\n", System.lineSeparator()));
+		JOptionPane op = new JOptionPane(pPnl, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+		JDialog dlg = op.createDialog(title.replace("\n", System.lineSeparator()));
+
+		// Wire up FocusListener to ensure JPasswordField is able to request focus when
+		// the dialog is first shown.
+		dlg.addWindowFocusListener(new WindowAdapter() {
+			@Override
+			public void windowGainedFocus(WindowEvent e) {
+				pPnl.gainedFocus();
+			}
+		});
+		
+		Supplier<char[]> getPassword = () -> {
+			dlg.setVisible(true);
+			dlg.dispose();
+			if (op.getValue() != null && op.getValue().equals(JOptionPane.OK_OPTION)) 
+				return pPnl.getPassword();
+			else return null;
+		};
+		
+		if (EventQueue.isDispatchThread()) {
+			return getPassword.get();
+		} else {
+			AtomicReference<char[]> ret = new AtomicReference<>();
+			try {
+				SwingUtilities.invokeAndWait(() -> {
+					ret.set(getPassword.get());
+				});
+				return ret.getAcquire();
+			} catch (Exception e) {
+				error("Exception in Thread working(SwingWorker)", "%e%", (e instanceof InvocationTargetException) ? (Exception)e.getCause() : e, false);
+				return null;
+			}
+			
+		}
+		
+	}
+	
 	private static JDialog createDialog() {
 		JDialog dialog = new JDialog();
 		dialog.setAlwaysOnTop(true);
 		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		return dialog;
 	}
+}
+
+
+/**
+ * Auto-focused password input JPanel with prompt from 
+ * <a href=https://stackoverflow.com/a/8881370>https://stackoverflow.com/a/8881370</a>
+ *  */
+class PasswordPanel extends JPanel {
+	
+	private static final long serialVersionUID = 3580352895097705874L;
+	private final JPasswordField passwordField = new JPasswordField();
+	private boolean gainedFocusBefore;
+
+	
+	public PasswordPanel(String prompt) {
+		super(new BorderLayout(0, 5));
+		add(new JLabel(prompt), BorderLayout.NORTH);
+		add(passwordField, BorderLayout.SOUTH);
+	}
+	/**
+	 * "Hook" method that causes the JPasswordField to request focus the first time
+	 * this method is called.
+	 */
+	void gainedFocus() {
+		if (!gainedFocusBefore) {
+			gainedFocusBefore = true;
+			passwordField.requestFocusInWindow();
+		}
+	}
+	public char[] getPassword() {
+		return passwordField.getPassword();
+	}
+
 }

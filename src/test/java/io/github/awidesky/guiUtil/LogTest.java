efe305fd0316b2a9401cd8c61577aac53dba25f4
Eugene Hong <awidesky@naver.com>
2024-05-03 08:26:22 +0900

    fix levelIterateTest
    Don't have to test for every Logger type, since log leveling is managed
    at AbstractLogger

--- src/test/java/io/github/awidesky/guiUtil/LogTest.java
+++ src/test/java/io/github/awidesky/guiUtil/LogTest.java
@@ -24,75 +24,51 @@ class LogTest {
 
 	@Test
 	void testLevel() {
-		levelIterateTest(SimpleLoggerTestAggregate::new);
-	}
-
-	void levelIterateTest(Supplier<LoggerTestAggregate> logGenerator) {
-		debugLogger.debug("Level Iterate Test for " + logGenerator.get().getLogger().getClass().getName());
-		debugLogger.debug();
-		
 		Arrays.stream(Level.values())
 			.forEach(eachLevel -> {
-				LoggerTestAggregate logGobbler = logGenerator.get();
-				Logger logger = logGobbler.getLogger();
-				logger.setLogLevel(eachLevel);
-				Map.of(
-					Level.INFO, logger.isInfoEnabled(),
-					Level.DEBUG, logger.isDebugEnabled(),
-					Level.TRACE, logger.isTraceEnabled(),
-					Level.WARNING, logger.isWarningEnabled(),
-					Level.ERROR, logger.isErrorEnabled(),
-					Level.FATAL, logger.isFatalEnabled()
-				).entrySet().stream()
-				.forEach(e -> {
-					assertEquals(eachLevel.includes(e.getKey()), e.getValue().booleanValue(),
-							() -> "level " + e.getKey() + " must be " + eachLevel.includes(e.getKey()) + ", since set level is : " + eachLevel);
-					assertEquals(e.getKey().ordinal() <= eachLevel.ordinal(), e.getValue().booleanValue(),
-							() -> "level " + e.getKey().ordinal() + "(" + e.getKey() + ") <= " + "level " + eachLevel.ordinal() + "(" + eachLevel
-								+ ") must be true, since set level is : " + eachLevel);
-				});
-				
-				Arrays.stream(Level.values()).forEach(le -> logger.logInLevel(le, le.name()));
-				
-				String output = logGobbler.getOutput();
-				Stream<String> outputStream = output.lines().map(str -> str.split(" ")[1]);
-				String nonmatch = outputStream.map(Level::valueOf).filter(l -> !eachLevel.includes(l)).map(Level::name).collect(Collectors.joining(", "));
-				
-				debugLogger.debug("Test for " + logger.toString());
-				output.lines().forEach(debugLogger::debug);
-				debugLogger.debug("========================================");
-				debugLogger.debug();
-				
-				
-				if(!"".equals(nonmatch)) {
-					System.out.println("Logger \"" + logger.toString() + "\"'s level is " + logger.getLogLevel());
-					System.out.println("========================================");
-					System.out.println(output);
-					System.out.println("========================================");
-					System.out.println("\"" + nonmatch + "\" should not printed!");
-					fail("\"" + nonmatch + "\" should not printed since logger level is " + logger.getLogLevel());;
-				}
+				StringWriter sw = new StringWriter();
+				levelIterateTest(eachLevel, new SimpleLogger(sw, true), sw::toString);
 			});
 	}
 
-}
+	void levelIterateTest(Level eachLevel, Logger logger, Supplier<String> logGobbler) {
+		logger.setLogLevel(eachLevel);
+		Map.of(
+				Level.INFO, logger.isInfoEnabled(),
+				Level.DEBUG, logger.isDebugEnabled(),
+				Level.TRACE, logger.isTraceEnabled(),
+				Level.WARNING, logger.isWarningEnabled(),
+				Level.ERROR, logger.isErrorEnabled(),
+				Level.FATAL, logger.isFatalEnabled()
+				).entrySet().stream()
+		.forEach(e -> {
+			assertEquals(eachLevel.includes(e.getKey()), e.getValue().booleanValue(),
+					() -> "level " + e.getKey() + " must be " + eachLevel.includes(e.getKey()) + ", since set level is : " + eachLevel);
+			assertEquals(e.getKey().ordinal() <= eachLevel.ordinal(), e.getValue().booleanValue(),
+					() -> "level " + e.getKey().ordinal() + "(" + e.getKey() + ") <= " + "level " + eachLevel.ordinal() + "(" + eachLevel
+					+ ") must be true, since set level is : " + eachLevel);
+		});
+
+		Arrays.stream(Level.values()).forEach(le -> logger.logInLevel(le, le.name()));
+
+		String output = logGobbler.get();
+		Stream<String> outputStream = output.lines().map(str -> str.split(" ")[1]);
+		String nonmatch = outputStream.map(Level::valueOf).filter(l -> !eachLevel.includes(l)).map(Level::name).collect(Collectors.joining(", "));
+
+		debugLogger.debug("Level Iterate Test for " + logger.toString());
+		output.lines().forEach(debugLogger::debug);
+		debugLogger.debug("========================================");
+		debugLogger.debug();
 
 
-interface LoggerTestAggregate {
-	public Logger getLogger();
-	public String getOutput();
-}
-
-class SimpleLoggerTestAggregate implements LoggerTestAggregate {
-	StringWriter sw = new StringWriter();
-	
-	@Override
-	public Logger getLogger() {
-		return new SimpleLogger(sw, true);
+		if(!"".equals(nonmatch)) {
+			System.out.println("Logger \"" + logger.toString() + "\"'s level is " + logger.getLogLevel());
+			System.out.println("========================================");
+			System.out.println(output);
+			System.out.println("========================================");
+			System.out.println("\"" + nonmatch + "\" should not printed!");
+			fail("\"" + nonmatch + "\" should not printed since logger level is " + logger.getLogLevel());;
+		}
 	}
-	
-	@Override
-	public String getOutput() {
-		return sw.toString();
-	}
+
 }

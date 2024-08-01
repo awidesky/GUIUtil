cecad40787583417c986f95225f945f0deee3f69
Eugene Hong <awidesky@naver.com>
2024-08-01 12:01:16 +0900

    add consoleLogger

---
+++ src/main/java/io/github/awidesky/guiUtil/ConsoleLogger.java
@@ -0,0 +1,27 @@
+package io.github.awidesky.guiUtil;
+
+/**
+ * Logs to the console via {@code System.out}.
+ */
+public class ConsoleLogger extends SimpleLogger {
+
+	/**
+	 * Constructs a console logger with auto flush enabled.
+	 */
+	public ConsoleLogger() {
+		this(true);
+	}
+	/**
+	 * Constructs a console logger with given auto flush option.
+	 */
+	public ConsoleLogger(boolean autoFlush) {
+		super(System.out, autoFlush);
+	}
+	
+	/**
+	 * Do noting because we shouldn't close {@code System.out}.
+	 */
+	@Override
+	public void close() {}
+
+}

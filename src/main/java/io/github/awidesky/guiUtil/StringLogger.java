958b0df1c825cf59fa70d09a657ba4c87ae2dbe7
Eugene Hong <awidesky@naver.com>
2024-08-17 18:50:31 +0900

    use synchronized list in StringLogger

--- src/main/java/io/github/awidesky/guiUtil/StringLogger.java
+++ src/main/java/io/github/awidesky/guiUtil/StringLogger.java
@@ -1,49 +1,74 @@
 package io.github.awidesky.guiUtil;
 
-import java.io.StringWriter;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import io.github.awidesky.guiUtil.level.Level;
 
 /**
- * Logs in a {@code StringWriter}, so that every log output can be collected into a buffer.
+ * Gather logs in a internal buffer.
  * {@code StringLogger#getString()} will return the logged data as {@code String}.
  * <br>
  * Field {@code printLogLevel} is set to {@code false} initially, since {@code StringLogger} 
  * is normally used to gobble output, like {@code java.io.StringWriter}.
  */
-public class StringLogger extends SimpleLogger {
+public class StringLogger extends AbstractLogger {
 
-	private StringWriter sw;
+	private List<String> list = Collections.synchronizedList(new LinkedList<>());
 	
 	/**
-	 * Creates a new StringLogger, without automatic line flushing.
+	 * Creates a new StringLogger, without automatic line flushing,
+	 * and with {@code printLogLevel} set to {@code false}.
 	 */
 	public StringLogger() {
 		this(false);
 	}
 	/**
-	 * Creates a new StringLogger.
+	 * Creates a new StringLogger,
+	 * with {@code printLogLevel} set to {@code false}. 
 	 */
 	public StringLogger(boolean autoFlush) {
-		this(new StringWriter(), autoFlush);
-	}
-	
-	private StringLogger(StringWriter s, boolean autoFlush) {
-		super(s, true);
-		sw = s;
 		setPrintLogLevel(false);
 	}
 	
 	/**
 	 * Returns the log string that were collected
-	 * (line separator is {@code System#lineSeparator()}).
 	 * After this call, the log buffer will cleared.
 	 * 
 	 * @return Texts that are logged since last call of this method.
 	 */
 	public String getString() {
-		String ret = sw.toString();
-		StringBuffer sb = sw.getBuffer();
-		sb.setLength(0);
-		sb.trimToSize();
+		String ret = list.stream().collect(Collectors.joining("\n"));
+		clear();
 		return ret;
+	}
+	
+	/**
+	 * Removes all of the logs from this logger.
+	 * {@code getString()} will return empty String
+	 * after this call returns.
+	 */
+	public void clear() {
+		list.clear();
+	}
+	
+	@Override
+	public void newLine() {
+		list.add("\n");
+	}
+	
+	/**
+	 * Does not do anything.<p>
+	 * If clearing inner buffer of the {@code StringLogger} is needed,
+	 * call {@code StringLogger#clear()}.
+	 */
+	@Override
+	public void close() {}
+	
+	@Override
+	protected void writeString(Level level, CharSequence str) {
+		list.add(getPrefix(level) + str);
 	}
 }
